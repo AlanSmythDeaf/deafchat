79cfb7e96d6832c9695da209e73ad9e729afb130
Alan Smyth <alan_smyth@hotmail.com>
2024-08-18 22:06:33 +0000

    Style the contact form

--- contact/forms.py
+++ contact/forms.py
@@ -3,7 +3,26 @@ from .models import ContactForm
 
 
 class ContactForm(forms.ModelForm):
+        name = forms.CharField(
+        label='Name',
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your name'})
+        )
+        phonenumber = forms.CharField(
+            label='Phone Number',
+            max_length=20,
+            widget=forms.TextInput(attrs={'placeholder': 'Enter your phone number'})
+        )
+        email = forms.EmailField(
+            label='Email',
+            widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+        )
+        message = forms.CharField(
+            label='Message',
+            widget=forms.Textarea(attrs={'placeholder': 'Write your message here'})
+        )
+
     # Contact from model ContactForm 
-    class Meta:
-        model = ContactForm
-        fields = ['name', 'phonenumber', 'email', 'message']
+        class Meta:
+            model = ContactForm
+            fields = ['name', 'phonenumber', 'email', 'message']
