355db6b4724b2aee2f25ffcd4bf1df9e853777c4
Alan Smyth <alan_smyth@hotmail.com>
2024-08-27 20:16:21 +0000

    Change colour for post and post link and change text in sign in and up

--- chat/settings.py
+++ chat/settings.py
@@ -16,6 +16,9 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+
+import cloudinary
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -61,6 +64,10 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+cloudinary.config(
+    secure=True,
+)
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
