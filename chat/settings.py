ba2c5a0c7e47ffd57cc00ba0ec459f443a0b81a4
Alan Smyth <alan_smyth@hotmail.com>
2024-08-27 22:14:42 +0000

    Add hero image, change the image flash

--- chat/settings.py
+++ chat/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-rz)jw@hcncll!*j)8m2#hw!mi$-aa7_s^b-cfl3c3^770!x(b8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-alansmythdeaf-deafchat-mm13edmzyws.ws.codeinstitute-ide.net",
                 ".herokuapp.com"
