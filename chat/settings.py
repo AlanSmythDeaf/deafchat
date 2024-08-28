a0f67c9c7f5aa16795544ee44a11b03731adfff8
Alan Smyth <alan_smyth@hotmail.com>
2024-08-28 11:17:40 +0000

    update the settings for secretkey

--- chat/settings.py
+++ chat/settings.py
@@ -28,7 +28,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rz)jw@hcncll!*j)8m2#hw!mi$-aa7_s^b-cfl3c3^770!x(b8'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
