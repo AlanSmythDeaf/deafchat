98f6708e41a7b2fd5e3192f4bfba8b2f1d5f626e
Alan Smyth <alan_smyth@hotmail.com>
2024-08-28 10:43:36 +0000

    Adding key

--- chat/settings.py
+++ chat/settings.py
@@ -29,6 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rz)jw@hcncll!*j)8m2#hw!mi$-aa7_s^b-cfl3c3^770!x(b8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
